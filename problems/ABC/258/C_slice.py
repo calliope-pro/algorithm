d13b9cc8ba42e72eeb9a45a7adeac1c61aa2381b
calliope <calliope183@gmail.com>
2022-07-24 16:16:03 +0900

    add

---
+++ problems/ABC/258/C_slice.py
@@ -0,0 +1,14 @@
+import sys
+
+rr = lambda: sys.stdin.readline().rstrip()
+rm = lambda: map(int, sys.stdin.readline().split())
+
+n, q = rm()
+s = rr()
+cut = 0
+for _ in range(q):
+    idx, x = rm()
+    if idx == 1:
+        cut = (cut + x) % n
+    else:
+        print(s[(-cut+x-1)%n])
