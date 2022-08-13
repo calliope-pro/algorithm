5c5ab7f06a39855e3b51b552f058640d2aaad4c7
calliope <calliope183@gmail.com>
2022-08-14 04:05:09 +0900

    add

--- common/__ABCLibray.py
+++ common/__ABCLibray.py
@@ -1,35 +1,77 @@
 import math
-from typing import List
 
 def calc_triangle_area(x0, y0, x1, y1, x2, y2):
     area = (x1 - x0)*(y2 - y0) - (x2 - x0)*(y1 - y0)
     area = 1/2 * abs(area)
     return area
 
-def get_prime_list(n: int) -> List[int]:
-    '''
-    エラトステネスの篩を用いた素数列挙
+class PrimeManager:
+    from typing import List
+    @classmethod
+    def get_prime_list(cls, n: int) -> List[int]:
+        '''
+        エラトステネスの篩を用いた素数列挙
 
-    Parameters
-    ----------
-    n : int
-        最大値
-    Returns
-    -------
-    List[int]
-        2 ~ nにおける素数リスト
-    '''
-    assert n >= 1, '`n` must be 1 or more.'
-    prime_list = []
-    is_prime_list = [True] * (n+1)
-    is_prime_list[0] = False
-    is_prime_list[1] = False
-    for i in range(2, n+1):
-        if is_prime_list[i]:
-            prime_list.append(i)
-            for j in range(i, n+1, i):
-                is_prime_list[j] = False
-    return prime_list
+        Parameters
+        ----------
+        n : int
+            最大値
+
+        Returns
+        -------
+        List[int]
+            2 ~ nにおける素数リスト
+
+        Raises
+        ------
+        AssertionError
+            `n`が0以下の場合
+        '''
+        assert n >= 1, '`n` must be 1 or more.'
+        prime_list = []
+        is_prime_list = [True] * (n+1)
+        is_prime_list[0] = False
+        is_prime_list[1] = False
+        for i in range(2, n+1):
+            if is_prime_list[i]:
+                prime_list.append(i)
+                for j in range(i, n+1, i):
+                    is_prime_list[j] = False
+        return prime_list
+    
+    @classmethod
+    def prime_factorization(cls, n: int, *, is_decoded: bool=False) -> List[int]:
+        '''
+        素因数分解し、素因数列挙
+
+        Parameters
+        ----------
+        n : int
+            素因数分解する数
+        is_decoded : bool, optional
+            展開するか, by default False
+
+        Returns
+        -------
+        List[int]
+            素因数のリスト
+            展開している場合は各々の素因数の個数入っている
+        '''
+        prime_list = []
+        ps = cls.get_prime_list(math.ceil(n**0.5) + 1)
+        for psv in ps:
+            if n % psv != 0:
+                continue
+            while n % psv == 0:
+                n //= psv
+                if is_decoded:
+                    prime_list.append(psv)
+            if not is_decoded:
+                prime_list.append(psv)
+        else:
+            if n != 1:
+                prime_list.append(n)
+        return prime_list
 
 class UnionFind:
     '''UnionFind
