062a504a6ab25eaef80e39618b67b29c0d66df0d
calliope <calliope183@gmail.com>
2022-08-15 00:34:37 +0900

    simplify

--- problems/AGC/040/A.py
+++ problems/AGC/040/A.py
@@ -11,9 +11,7 @@ rle = [(key, len(list(li))) for key, li in itertools.groupby(s)]
 ans = 0 if rle[0][0] == '<' else rle[0][1]
 for idx in range(len(rle)):
     a, b = rle[idx], rle[min(idx + 1, len(rle)-1)]
+    ans += ((a[1] - 1) * a[1]) // 2
     if a[0] == '<':
-        ans += ((a[1] - 1) * a[1]) // 2
         ans += max(a[1], b[1])
-    else:
-        ans += ((a[1] - 1) * a[1]) // 2
 print(ans)
