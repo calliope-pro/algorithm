3bc85b91bacd51b3a02aab44d55d6fc3ede9190c
calliope <calliope183@gmail.com>
2022-08-28 00:35:46 +0900

    change architecture

--- common/libs/__ABCLibray.py
+++ common/libs/__ABCLibray.py
@@ -1,11 +1,6 @@
 import math
 from typing import List, Sequence
 
-def calc_triangle_area(x0, y0, x1, y1, x2, y2):
-    area = (x1 - x0)*(y2 - y0) - (x2 - x0)*(y1 - y0)
-    area = 1/2 * abs(area)
-    return area
-
 def rotate90(m: Sequence[Sequence]) -> List[tuple]:
     '''
     二次元の配列やシーケンスを右に90度回転
@@ -22,7 +17,52 @@ def rotate90(m: Sequence[Sequence]) -> List[tuple]:
     '''
     return list(zip(*m[::-1]))
 
-class PrimeManager:
+class GeometryHandler:
+    @classmethod
+    def calc_triangle_area(cls, x0, y0, x1, y1, x2, y2) -> int:
+        '''
+        3点より三角形の面積を算出
+
+        Parameters
+        ----------
+        x0, y0 : int
+            点(x0, y0)となる
+        x1, y1 : int
+            点(x1, y1)となる
+        x2, y2 : int
+            点(x2, y2)となる
+
+        Returns
+        -------
+        int
+            三角形の面積
+        '''
+        area = (x1 - x0)*(y2 - y0) - (x2 - x0)*(y1 - y0)
+        area = 1/2 * abs(area)
+        return area
+
+    @classmethod
+    def is_convex(cls, x0, y0, x1, y1) -> bool:
+        '''
+        隣接する2辺に関する凸性判定
+
+        Parameters
+        ----------
+        x0, y0 : int
+            0度の基準となる辺・ベクトル
+            (x0, y0)となる辺・ベクトル
+        x1, y1 : int
+            (x1, y1)となる辺・ベクトル
+
+        Returns
+        -------
+        bool
+            凸性の有無
+            (x0, y0)から反時計回りに180度以上ならば凸性が認められずFalse
+        '''
+        return (x0*y1 - x1*y0) > 0
+
+class PrimeHandler:
     @classmethod
     def get_prime_list(cls, n: int) -> List[int]:
         '''
@@ -262,11 +302,3 @@ class SegTree:
         if query_right == -1:
             query_right = (self.nodes_length + 1) // 2
         return self._get(query_left, query_right)
-
-
-
-
-
-
-
-
