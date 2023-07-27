e240d24e06adafbf675a830ee9b281fdf4d9b040
calliope <calliope183@gmail.com>
2023-07-27 13:28:27 +0900

    add

---
+++ problems/ABC/310/A.py
@@ -0,0 +1,10 @@
+# ABC310A
+# minを使えば余裕
+import sys
+
+rm = lambda: map(int, sys.stdin.readline().split())
+rl = lambda: list(map(int, sys.stdin.readline().split()))
+
+n, p, q = rm()
+dl = rl()
+print(min(p, min(dl) + q))
---
+++ problems/ABC/310/B.py
@@ -0,0 +1,18 @@
+# ABC310B
+# setとitertools.permutationsを使うと簡単に解ける
+import sys
+import itertools
+
+rm = lambda: map(int, sys.stdin.readline().split())
+
+n, m = rm()
+items = []
+for _ in range(n):
+    p, c, *f = rm()
+    items.append((p, set(f)))
+
+for (item_i, item_j) in itertools.permutations(items, 2):
+    if item_i[0] >= item_j[0] and item_i[1] <= item_j[1] and ((len(item_j[1])>len(item_i[1])) or item_i[0] > item_j[0]):
+        print('Yes')
+        exit()
+print('No')
